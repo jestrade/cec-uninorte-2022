bd9d641cb8884dea4c158010cf99fb9e82fa5c08
Jesus Estrada <jesuestrada@gmail.com>
2022-06-15 18:57:03 -0500

    arreglos y objetos

--- 03-arreglos.js
+++ 03-arreglos.js
@@ -4,7 +4,22 @@ const arregloDeTipos = [1, true, 2, "jesus", false, ["estrada"]];
 
 console.log("nombres", nombres);
 console.log("primer nombre agregado:", nombres[0]);
-console.log("ultimo nombre agregado:", nombres[3]);
+console.log("último nombre agregado:", nombres[3]);
 console.log("arregloDeTipos", arregloDeTipos);
 console.log("tamaño arregloDeTipos", arregloDeTipos.length);
-console.log("ultima posición arregloDeTipos", arregloDeTipos[arregloDeTipos.length - 1]);
+console.log("última posición arregloDeTipos", arregloDeTipos[arregloDeTipos.length - 1]);
+
+// ciclos: while + for
+
+let indice = 0;
+const tamanoArreglo = nombres.length;
+while (indice < tamanoArreglo) {
+    console.log(`elemento en la posición ${indice} es ${nombres[indice]}`);
+    indice++; // indice = indice + 1;
+}
+console.log("fin");
+
+//for (inicializar el índice; condición de salida; incremento)
+for (let indice = 0; indice < tamanoArreglo; indice++) {
+    console.log(`elemento en la posición ${indice} es ${nombres[indice]}`);
+}
---
+++ 04-objetos.js
@@ -0,0 +1,89 @@
+const persona1 = {
+    primerNombre: "Ana",
+    segundoNombre: "María",
+    primerApellido: "Pérez",
+    segundoApellido: "Leyva",
+    sexo: "F",
+    edad: 19,
+    paisNacimiento: "Colombia",
+    direcciones: [
+        {
+            tipo: "Casa",
+            direccion: "Kra 123"
+        },
+        {
+            tipo: "Trabajo",
+            direccion: "Calle 987"
+        }
+    ],
+    telefonos: [
+        {
+            tipo: "Celular",
+            indicativo: 57,
+            telefono: 3001234567
+        },
+        {
+            tipo: "Fijo",
+            indicativo: 57,
+            telefono: 6051234567
+        }
+    ]
+};
+
+const persona2 = {
+    primerNombre: "Juan",
+    segundoNombre: "Gomez",
+    primerApellido: "Pérez",
+    segundoApellido: "Leyva",
+    sexo: "M",
+    edad: 19,
+    paisNacimiento: "Colombia",
+    direcciones: [
+        {
+            tipo: "Casa",
+            direccion: "Kra 123"
+        },
+        {
+            tipo: "Trabajo",
+            direccion: "Calle 987"
+        }
+    ],
+    telefonos: [
+        {
+            tipo: "Celular",
+            indicativo: 57,
+            telefono: 3001234568
+        },
+        {
+            tipo: "Fijo",
+            indicativo: 57,
+            telefono: 6051234567
+        }
+    ]
+};
+
+console.log("nombre persona 1", persona1.primerNombre);
+const nombreCompletoPersona1 = `${persona1.primerNombre} ${persona1.segundoNombre} ${persona1.primerApellido} ${persona1.segundoApellido}`;
+console.log("nombre completo persona 1:", nombreCompletoPersona1);
+
+// acceder a cada atributo del objeto persona 1
+for (let key in persona1) {
+    console.log(`en el atributo ${key} del objeto persona1 el valor es: ${persona1[key]}`);
+}
+
+
+// Parte 2
+const personas = [persona1, persona2];
+
+const numeroPersonas = personas.length;
+for (let indice = 0; indice < numeroPersonas; indice++) {
+    const nombre = `${personas[indice].primerNombre} ${personas[indice].primerApellido}`;
+    console.log(`El nombre de la persona ${indice} es: ${nombre}`);
+}
+
+for (let indice = 0; indice < numeroPersonas; indice++) {
+    for (let key in personas[indice]) {
+        console.log(`en el atributo ${key} de la persona en la posición ${indice} el valor es: ${personas[indice][key]}`);
+    }
+}
+
